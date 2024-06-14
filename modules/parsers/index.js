0ab00029b8d287ed9bc6d66c5d3cd50273ab86cc
Andrew Top <top@adobe.com>
2024-06-14 12:27:11 -0400

    Text - block - might have to work on this a little more to explain it means immediate text children - TEXT is default content usually

--- modules/parsers/index.js
+++ modules/parsers/index.js
@@ -1,5 +1,6 @@
 import parseMetadata from './metadata.js';
 import parseColumns from './columns.js';
+import parseText from './text.js';
 import defaultParser from './block.js';
 import parseCarousel from './carousel.js';
 
@@ -7,6 +8,7 @@ const parsers = {
   metadata: parseMetadata,
   columns: parseColumns,
   carousel: parseCarousel,
+  text: parseText,
   block: defaultParser,
 };
 
